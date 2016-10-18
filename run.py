f7ad5e0e16bc045d3f9c54d3287f066b026899e4
codeWARRIOR <mugapedia@gmail.com>
2016-10-18 17:13:25 +0300

    Adds file that starts application on a development server

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from app import create_app, db
+from app.models import User
+
+if __name__ == '__main__':
+    app = create_app('development')
+    with app.app_context():
+        db.create_all()
+        if User.query.filter_by(username='codeWARRIOR').first() is None:
+            User.create_user('Luke', 'mugapedia@gmail.com', 'codeWARRIOR', 'lpi02H6q6F', True)
+    app.run()
