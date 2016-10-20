c1db9f699111f1ecb1b3ba9199245980e59291c4
codeWARRIOR <mugapedia@gmail.com>
2016-10-20 13:47:26 +0300

    Adds routes for handling assets views

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_required, login_user, logout_user, current_user
-from ..models import User
+from ..models import User, Asset
 from . import main
-from .forms import LoginForm, UserCreationForm
+from .forms import LoginForm, UserCreationForm, AssetCreationForm
 from functools import wraps
 
 
@@ -41,6 +41,7 @@ def logout():
 @login_required
 def dashboard():
     return render_template('dashboard.html')
+
 
 def check_is_super_admin(func):
     @wraps(func)
@@ -110,3 +111,37 @@ def create_user():
 def page_not_found(e):
     return render_template('templates/404.html')
 
+
+@main.route('/users')
+def users():
+    users = User.find_all()
+    return render_template('users.html', users=users)
+
+
+@main.route('/create_asset', methods=['GET', 'POST'])
+@login_required
+@check_is_admin
+def create_asset():
+    form = AssetCreationForm()
+    if form.validate_on_submit():
+        asset = Asset.query.filter_by(serial_no=form.serial_no.data).first()
+        if asset is not None:
+            flash('an asset with a similar serial number already exists')
+            return redirect(url_for('main.create_asset', **request.args))
+        name = form.name.data
+        description = form.description.data
+        serial_no = form.serial_no.data
+        andela_code = form.andela_code.data
+        bought = form.bought.data
+        cost = form.cost.data
+        Asset.create_asset(name, description, serial_no,
+                           andela_code, bought, cost)
+        flash("Asset created")
+        return redirect(request.args.get('next') or url_for('main.assets'))
+    return render_template('create_asset.html', form=form)
+
+
+@main.route('/assets')
+def assets():
+    assets = Asset.find_all()
+    return render_template('assets.html', assets=assets)
