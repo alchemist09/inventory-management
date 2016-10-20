ca7664e746f709b4ac3e1a10d860eed827d11cb6
codeWARRIOR <mugapedia@gmail.com>
2016-10-20 13:48:44 +0300

    Adds forms used for adding assets

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextField, DateField
 from wtforms.validators import Required, Length
 
 
@@ -12,8 +12,21 @@ class LoginForm(Form):
 
 class UserCreationForm(Form):
     name = StringField('Name', validators=[Required(), Length(1, 20)])
-    email = StringField('Email', validators=[Required(), Length(1, 20)])
+    email = StringField('Email', validators=[Required(), Length(1, 40)])
     username = StringField('Username', validators=[Required(), Length(1, 20)])
-    password = StringField('Password', validators=[Required(), Length(1, 20)])
+    password = PasswordField('Password', validators=[
+                             Required(), Length(1, 20)])
     is_admin = BooleanField('Is Admin')
+    submit = SubmitField('Submit')
+
+
+class AssetCreationForm(Form):
+    name = StringField('Item Name', validators=[Required(), Length(3, 40)])
+    description = TextField('description')
+    serial_no = StringField('Serial  Number', validators=[
+                            Required(), Length(3, 40)])
+    andela_code = StringField('Andela Code', validators=[
+                              Required(), Length(3, 64)])
+    bought = DateField('Date Bought', validators=[Required()])
+    cost = StringField('Cost(Ksh)', validators=[Required()])
     submit = SubmitField('Submit')
