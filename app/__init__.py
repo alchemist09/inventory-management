7652820d27e9f264e1f8dbb959e962466c740b1a
codeWARRIOR <mugapedia@gmail.com>
2016-10-20 17:35:59 +0300

    Revert "Migration to alter database schema of assets table"
    This reverts commit 4373e923691a303abc41c123489bc186385535e5.

--- app/__init__.py
+++ app/__init__.py
@@ -3,37 +3,27 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_admin import Admin
-from flask_login import current_user
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 lm = LoginManager()
 lm.login_view = 'main.index'
-admin = Admin()
 
 
 def create_app(config_name):
-    """
-        Create an application instance.
-        
-        This function acts as a factory function that
-        can create different flask app instances based
-        on the configuration settings it is passed
-    """
+    """Create an application instance."""
     app = Flask(__name__)
 
-    # import configuration for the application from config folder
+    # import configuration
     cfg = os.path.join(os.getcwd(), 'config', config_name + '.py')
     app.config.from_pyfile(cfg)
 
-    # initialize flask extensions on the application instance
+    # initialize extensions
     bootstrap.init_app(app)
     db.init_app(app)
     lm.init_app(app)
-    admin.init_app(app)
 
-    # import blueprints and register them on application's instance
+    # import blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
